4336c4db6dc7a0e40bfec778c579cdb694d0e841
Arun Parmar <46495338+Arunpar@users.noreply.github.com>
2019-02-02 14:25:32 +0530

    Add files via upload

---
+++ Temperature_Conversion.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	float ferenhite,celcius;
+	cout<<"Enter the temperature in celcius : ";
+	cin>>celcius;
+	ferenhite=(celcius*9/5)+32;
+	cout<<"The temperature in ferenhite is : "<<ferenhite;
+	return 0;
+	
+}
