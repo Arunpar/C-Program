e07cf9f51ccdde1ec6d0e739a002d4da645d02e9
Arun Parmar <46495338+Arunpar@users.noreply.github.com>
2019-02-02 15:35:58 +0530

    Add files via upload

---
+++ Check_Whethe_Number_Is_Even_Or_Odd.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main()
+{
+int num;
+cout<<"Enter the Number : ";
+cin>>num;
+if(num%2==0)
+cout<<num<<" is Even Number";
+else
+cout<<num<<" is Odd Number";
+return 0;
+}
