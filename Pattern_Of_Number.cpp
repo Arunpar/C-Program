064be8a8351528f34d3b61341159e5a5b6ae1b51
Arun Parmar <46495338+Arunpar@users.noreply.github.com>
2019-02-07 22:31:09 +0530

    Add files via upload

---
+++ Pattern_Of_Number.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int row,col;
+		for(row=1;row<=10;row++)
+	{
+	for(col=1;col<=row;col++)
+	{
+		cout<<" "<<col;
+	}
+	cout<<"\n";
+	}
+	return 0;
+}
+
+
