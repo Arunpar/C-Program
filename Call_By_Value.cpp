701b4c3df2010cc75fdeefb3973c909b168d8fd9
Arun Parmar <46495338+Arunpar@users.noreply.github.com>
2019-02-19 22:27:47 +0530

    Add files via upload

---
+++ Call_By_Value.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+void swap(int,int);
+main()
+{
+	int num1;
+	int num2;
+	cout<<"Enter the Two Numbers :\n";
+	cin>>num1>>num2;
+	cout<<"The Original Numbers are :\n";
+	cout<<num1<<"\n"<<num2<<"\n";
+	swap(num1,num2);
+}
+void swap(int num1,int num2)
+{
+	int temp;
+	temp=num1;
+	num1=num2;
+	num2=temp;
+	cout<<"The Swapped Numbers are :\n";
+	cout<<num1<<"\n"<<num2<<"\n";
+}
+
